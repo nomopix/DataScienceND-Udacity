792ff9e218d31ed41ca7aafbb4a4b3954098c3ef
Noemi <noemi@upuno.com>
2019-11-04 15:08:31 +0000

    Finished functional ETL py

--- Disaster-response-pipelines/data/process_data.py
+++ Disaster-response-pipelines/data/process_data.py
@@ -1,16 +1,66 @@
 import sys
+import pandas as pd
+import os
+import numpy as np
+from pandas.api.types import is_string_dtype
+import sqlalchemy as sqla
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+        input: 2 file full / relative paths: messages and categories csv files.
+        output: 1 dataframe with the merged data
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = pd.merge(messages, categories, on="id", how="left")
+    return df
 
 
 def clean_data(df):
-    pass
+    ''' 
+        input: a dataframe containing messages and categories
+        output: a dataframe with:
+            - columns correctly named
+            - values on the correct data type for purpose
+            - duplicates removed
 
+        id, message, original, genre, categories
+    '''
+    # First create a dataframe of the categories, keeping id
+    i = df.columns.get_loc('categories')
+    tempDF = df['categories'].str.split(";", expand = True)
+    categs = pd.concat([df.iloc[:, :i], tempDF, df.iloc[:, i+1:]], axis=1)
+
+    # extract column names the expanded columns
+    row = categs.iloc[0:1] # pick first row to gather column names
+    skip = ['id','message','original','genre']
+    category_colnames = list()
+    for item in row.iteritems():
+        if item[0] not in skip:
+            category_colnames.append(item[1][0].split("-")[0])
+    categs.columns = skip + category_colnames 
+
+    # convert to ints those new columns' values 
+    for column in categs:
+        if column not in skip:
+            # set each value to be the last character of the string
+            categs[column] = categs[column].str[-1]
+            # convert column from string to numeric
+            categs[column] = categs[column].astype(int)
+
+    # remove duplicates
+    df.drop_duplicates(inplace=True)
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    '''
+        input: database file name
+        output: none - file saved with name provided in data folder.
+    '''
+    engine = sqla.create_engine('sqlite:///'+database_filename)  
+    df.to_sql('clasified_messages', engine, index=False, if_exists = 'replace')
 
 
 def main():
--- Disaster-response-pipelines/readme.md
+++ Disaster-response-pipelines/readme.md
@@ -13,7 +13,7 @@
     - disaster_categories.csv  # data to process 
     - disaster_messages.csv  # data to process
     - process_data.py
-    - InsertDatabaseName.db   # database to save clean data to
+    - CleanMessages.db   # database to save clean data to
 - models
     - train_classifier.py
     - classifier.pkl  # saved model 
